180055b7093b4fe1ea6c8ffb15caea69acacfd93
Админ <@LAPTOP-V5M03OMQ>
2022-12-02 09:35:29 +0300

    Добавьте файлы проекта.

---
+++ DeliveryApp/Ord.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DeliveryApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Ord.xaml
+    /// </summary>
+    public partial class Ord : Window
+    {
+        public Ord()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthBack(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void SendForm(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Заказ оформлен");
+        }
+    }
+}
---
+++ DeliveryApp/Registration.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DeliveryApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void BackAuth(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void Registr(object sender, RoutedEventArgs e)
+        {
+            Ord ord = new Ord();
+            ord.Show();
+            Close();
+        }
+    }
+}
